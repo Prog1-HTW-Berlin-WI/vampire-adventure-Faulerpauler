bcdfe343f42e19c06b6958dcd878e8d06c3beedc
Paul <page.bailer@gmx.de>
2022-06-27 17:02:49 +0200

    Main menu erweitert & Menü Methoden angelegt

--- app/VampireAdventureApp.java
+++ app/VampireAdventureApp.java
@@ -7,9 +7,8 @@ import java.util.Scanner;
  *
  */
 public class VampireAdventureApp {
-    // Hallo
+
     private static Scanner scanner = new Scanner(System.in);
-    private String paul = "Hallo";
 
     /**
      * @param args mainklasse
@@ -50,14 +49,19 @@ public class VampireAdventureApp {
                 createVampire();
                 break;
             case 2:
+                showSelectedVampire();
                 break;
             case 3:
+                listAllVampires();
                 break;
             case 4:
+                deleteVampire();
                 break;
             case 5:
+                startNightlyAdventure();
                 break;
             case 6:
+                quit();
                 break;
             default: {
                 System.out.println("Invalid input. Please choose a correct number between 1 and 6");
@@ -73,7 +77,8 @@ public class VampireAdventureApp {
      */
     private static void showMenu() {
 
-        String menuItems[] = { "", "(1)\t Create Vampire", "(2)\t Show Selected Vampire" };
+        String menuItems[] = { "", "(1)\t Create Vampire", "(2)\t Show Selected Vampire", "(3)\t List all Vampires",
+                "(4)\t Delete Vampire", "(5)\t Start Nightly Adventure", "(6)\t Quit" };
 
         System.out.println("\nVampire Adventures 1.0\n");
         for (int i = 1; i < menuItems.length; i++) {
@@ -85,6 +90,31 @@ public class VampireAdventureApp {
     private static void createVampire() {
         System.out.println(
                 "\nLeider hat die Methode noch keinen Code. Aber Du kannst hoffentlich nachvollziehen wie der Ablauf ist. Hier sollst Du einen Vampir anlegen.\n");
+    }
+
+    private static void showSelectedVampire() {
+        System.out.println(
+                "\nLeider hat die Methode noch keinen Code. Aber Du kannst hoffentlich nachvollziehen wie der Ablauf ist. Hier sollst Du einen Vampir anlegen.\n");
+    }
+
+    private static void listAllVampires() {
+        System.out.println(
+                "\nLeider hat die Methode noch keinen Code. Aber Du kannst hoffentlich nachvollziehen wie der Ablauf ist. Hier sollst Du einen Vampir anlegen.\n");
+    }
+
+    private static void deleteVampire() {
+        System.out.println(
+                "\nLeider hat die Methode noch keinen Code. Aber Du kannst hoffentlich nachvollziehen wie der Ablauf ist. Hier sollst Du einen Vampir anlegen.\n");
+    }
+
+    private static void startNightlyAdventure() {
+        System.out.println(
+                "\nLeider hat die Methode noch keinen Code. Aber Du kannst hoffentlich nachvollziehen wie der Ablauf ist. Hier sollst Du einen Vampir anlegen.\n");
+    }
+
+    private static void quit() {
+        System.out.println("\nDas Spiel wird jetzt beendet, bis zum nächsten Mal!\n");
+        System.exit(0);
     }
 
 }
