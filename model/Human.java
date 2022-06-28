3208d547320b21f140e0e7eb6ae1cf0b1c3583b8
ProgMojo <m.moussa2422@gmail.com>
2022-06-28 23:59:19 +0200

    hihi

--- model/Human.java
+++ model/Human.java
@@ -1,6 +1,16 @@
 package model;
+import java.util.Random;
 
 public class Human {
     
     private String name;
+    private String id = UUID.randomUUID().toString();
+    private Random amountOfBlood =  // zwischen 6 und 8 - wenn weniger als 5 dann ists aus 
+
+    public Human(Random amountOfBlood){
+        this.amountOfBlood = amountOfBlood;
+
+    }
 }
+
+
--- model/Vampire.java
+++ model/Vampire.java
@@ -1,4 +1,42 @@
 package model;
+import java.util.Random;
+
 public class Vampire {
-    
+    private String id = UUID.randomUUID().toString();
+    private String name;
+    private int grandness; 
+    private int hunger = 5;
+    private Vampire creator = null;
+    private boolean isDrinkingBlood;
+    private boolean inFight;
+    private boolean canControlInstincts; // nach 10 Nächten true
+    private int energy = 10; // geringer als 10 -> finale Auslöschung
+    private boolean finallyDead; //unwiederbringlich tot  
+
+    public Vampire(String name, Vampire creator ){
+        this.name = name;
+        this.creator = creator; 
+
+    }
+
+    public boolean attackHuman(Human human){
+        Random random = new Random();
+        int Zufallsergebnis = random.nextInt(100);
+        if(human <= 60){
+            return true;
+        }
+        if(human) > 60){
+            return false;
+        }
+    }
+
+    public boolean drinkBlood(){
+        if  = true;
+    }
+
+    public boolean takeDamage(int damage){
+        
+    }
+
+
 }
