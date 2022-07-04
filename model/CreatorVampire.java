607c39016313640edbdc882c0854be4036036664
Paul <page.bailer@gmx.de>
2022-07-04 20:07:30 +0200

    Creator Vampire Datentyp erstellt

---
+++ model/CreatorVampire.java
@@ -0,0 +1,28 @@
+package model;
+
+public class CreatorVampire extends Vampire {
+    // Attribute
+    private Vampire[] descendants;
+
+    // Konstruktor
+    public CreatorVampire(String name) {
+        this.name = name;
+    }
+
+    // Operationen
+    public void listAllDescendants() {
+        for (int i = 0; i < descendants.length; i++) {
+            System.out.println(descendants[i]);
+        }
+    }
+
+    // Getter & Setter
+    public Vampire[] getDescendants() {
+        return descendants;
+    }
+
+    public void setDescendants(Vampire[] descendants) {
+        this.descendants = descendants;
+    }
+
+}
